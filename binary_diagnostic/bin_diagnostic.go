f42a574633579f1102e97288dc8e9fa91a52b09a
Loirito <joaofranciscosantosferreira@gmail.com>
2021-12-06 17:50:22 +0000

    Day 3 is complete.

--- binary_diagnostic/bin_diagnostic.go
+++ binary_diagnostic/bin_diagnostic.go
@@ -20,11 +20,15 @@ func main(){
 	first_line := bf.Text()
 	var ones = make([]int, len(first_line))
 	var zeros = make([]int, len(first_line))
+	var oxy_gen_arr = []string{first_line}
+	var co2_arr = []string{first_line}
 
 	countBits(first_line, zeros, ones)
 
 	for bf.Scan(){
 		line := bf.Text()
+		oxy_gen_arr = append(oxy_gen_arr, line)
+		co2_arr = append(co2_arr, line)
 		countBits(line, zeros, ones)
 	}
 	
@@ -39,6 +43,25 @@ func main(){
 		}
 	}
 
+	for i:=0; i<len(first_line); i++{
+		oxy_ones, oxy_zeros := countRelevantBit(oxy_gen_arr, i)
+		co2_ones, co2_zeros := countRelevantBit(co2_arr, i)
+		if oxy_ones > oxy_zeros {
+			oxy_gen_arr = deleteUnwantedValues(oxy_gen_arr, i, '1')
+		} else if oxy_zeros > oxy_ones{
+			oxy_gen_arr = deleteUnwantedValues(oxy_gen_arr, i, '0')
+		} else{
+			oxy_gen_arr = deleteUnwantedValues(oxy_gen_arr, i, '1')
+		}
+		if co2_ones < co2_zeros{
+			co2_arr = deleteUnwantedValues(co2_arr, i, '1')
+		} else if co2_zeros < co2_ones{
+			co2_arr = deleteUnwantedValues(co2_arr, i, '0')
+		} else{
+			co2_arr = deleteUnwantedValues(co2_arr, i, '0')
+		}
+	}
+
 	fmt.Println("gamma rate binary=", gamma_rate_bin)
 	fmt.Println("epsilon rate binary=", epsilon_rate_bin)
 	gamma_rate_dec := convertBinToDec(gamma_rate_bin)
@@ -46,6 +69,16 @@ func main(){
 	fmt.Println("gamma rate decimal =", gamma_rate_dec)
 	fmt.Println("epsilon rate decimal =", epsilon_rate_dec)
 	fmt.Println("power consumption =", gamma_rate_dec*epsilon_rate_dec)
+	
+	// part 2 
+	fmt.Println("\noxygen gen. binary=", oxy_gen_arr[0])
+	fmt.Println("co2 scrubber binary=", co2_arr[0])
+	oxygen_gen_dec := convertBinToDec(oxy_gen_arr[0])
+	co2_scrubber_dec := convertBinToDec(co2_arr[0])
+	fmt.Println("oxygen gen. decimal =", oxygen_gen_dec)
+	fmt.Println("co2 scrubber decimal =", co2_scrubber_dec)
+	fmt.Printf("life support rating = %.0f\n", oxygen_gen_dec*co2_scrubber_dec)
+	
 }
 
 func countBits(str string, zero_arr []int, ones_arr []int){
@@ -68,4 +101,36 @@ func convertBinToDec(binary string) float64{
 		exp += 1
 	}
 	return val
+}
+
+// only used for part 2
+func countRelevantBit(arr []string, index int) (int, int){
+	ones := 0
+	zeros := 0
+	for i:=0; i<len(arr); i++{
+		if int32(arr[i][index]) == '1'{
+			ones++
+		} else{
+			zeros++
+		}
+	}
+	return ones, zeros
+}
+
+// value corresponds to the bit value we want to preserve
+func deleteUnwantedValues(arr []string, index int, value int32) []string{
+	i:=0
+	if len(arr) == 1{
+		return arr
+	}
+	for i<len(arr){
+		array_value := arr[i]
+		if int32(array_value[index]) == value{
+			i++
+		} else {
+			arr[i] = arr[len(arr)-1]
+			arr = arr[:len(arr)-1]
+		}
+	}
+	return arr
 }
